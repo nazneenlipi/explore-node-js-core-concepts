2551930f81f4c94ac988684a63f7933cbfd78cfd
nazneenlipi <lipinazneen65@gmail.com>
2025-05-31 12:59:26 +0600

    node js file system synchronous:"

--- module2/file-sytem.js
+++ module2/file-sytem.js
@@ -1,9 +1,9 @@
 const fs = require('node:fs');
 console.log('1')
-const text = "Learning file stystem2";
+const text = "Learning file stystem";
 
-fs.writeFileSync('/hello.txt', text);
+fs.writeFileSync('./hello.txt', text);
 console.log('3')
-const data = fs.readFileSync('/hello.txt', {encoding : "utf-8"})
-console.log('data', data)
+const data = fs.readFileSync('./hello.txt', {encoding : "utf-8"})
 console.log('4')
+console.log('data', data)
