dba553015dbde9838b77e041beec751b81c97fb5
nazneenlipi <lipinazneen65@gmail.com>
2025-06-02 00:49:55 +0600

    all todo

--- module2/todo-app/server.js
+++ module2/todo-app/server.js
@@ -1,7 +1,27 @@
 const http = require("http")
+const path = require("path")
+const fs = require("fs")
+
+const filePath = path.join(__dirname,"./db/todo.json")
 
 const server = http.createServer((req,res)=>{
-    res.end("Welcome to the todo app")
+    if(req.url === "/todos" && req.method === "GET"){
+        const data = fs.readFileSync(filePath , {encoding : "utf-8"})
+
+        res.writeHead(200, {
+            "content-type":"application/json",
+        })
+
+        res.end(
+            data
+        )
+        
+    }else if(req.url === "/todos/create-todo" && req.method === "POST"){
+        res.end("Todo created")
+    }else{
+    res.end("Route not found")
+    }
+   
 })
 
 server.listen(8000, "127.0.0.1", ()=>{
